2db0b7effe614ffda1ec4d89b362b8550d85fcb9
mj94 <mj94@vntgcorp.com>
2025-07-06 04:14:55 +0900

    feat(public) : favicon 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,13 @@ import ClientLayout from "@/components/ClientLayout"; // 새로 만들 클라이
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Testy - 나를 알아가는 특별한 여행",
+  title: "Testy - 가볍게 해보는 성향 테스트",
   description:
-    "간단한 질문들로 숨겨진 나의 모습을 발견해보세요. 성격, 연애, 진로 등 다양한 테스트를 제공합니다.",
+    "간단한 질문들로 숨겨진 나의 모습을 발견해보세요. 성격, 연애, 진로 등 다양한 테스트를 제공합니다. 가볍게 해보는 성향 테스트",
   generator: "v0.dev",
+  icons: {
+    icon: "/favicon.png", // 또는 "/favicon.ico"
+  },
 };
 
 export default function RootLayout({
