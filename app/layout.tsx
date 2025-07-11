f1f3261a2453983e62871a0be580c5dc3092498c
mj94 <mj94@vntgcorp.com>
2025-07-12 00:48:28 +0900

    feat(ga) : ga 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,42 @@
 import type React from "react";
 import "./globals.css"; // 전역 스타일시트
 
-// 앱 전체의 뼈대를 만드는 가장 기본적인 레이아웃입니다.
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="ko">
       <head>
+        {/* Google Analytics (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZLT3VML0DW"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZLT3VML0DW');
+            `,
+          }}
+        />
+
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N255D99T');`,
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N255D99T');`,
           }}
         />
+
+        {/* Verification Tags */}
         <meta
           name="google-site-verification"
           content="XwCW-t3PJfyPYJRWSItA6cY1Yoy3_zuBZ8N9CJEzZwc"
@@ -27,6 +45,8 @@ export default function RootLayout({
           name="naver-site-verification"
           content="4be26cfdfa67a9fe5f9f0b91758530293065fdb8"
         />
+
+        {/* Google Adsense */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6915584561138880"
@@ -34,6 +54,7 @@ export default function RootLayout({
         ></script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-N255D99T"
