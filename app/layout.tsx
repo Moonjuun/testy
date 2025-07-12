cadb4e2084c142d51428cd3eb63a174c7b65e0ed
mj94 <cmoonjun11@gmail.com>
2025-07-12 15:14:55 +0900

    feat(meta) : 스크립트 태그 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import "./globals.css"; // 전역 스타일시트
+import Script from "next/script"; // next/script 컴포넌트 임포트
 
 export default function RootLayout({
   children,
@@ -26,7 +27,28 @@ export default function RootLayout({
         />
 
         {/* Google Tag Manager */}
-        <script
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZLT3VML0DW"
+          strategy="afterInteractive" // 페이지가 상호작용 가능해진 후 스크립트 로드
+        />
+        {/* Google Analytics (gtag.js) - Inline Configuration */}
+        <Script
+          id="google-analytics-config" // 인라인 스크립트를 위한 고유 ID
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZLT3VML0DW');
+            `,
+          }}
+          strategy="afterInteractive" // 페이지가 상호작용 가능해진 후 스크립트 로드
+        />
+
+        {/* Google Tag Manager - Inline Script */}
+        <Script
+          id="google-tag-manager" // 인라인 스크립트를 위한 고유 ID
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -34,24 +56,15 @@ export default function RootLayout({
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-N255D99T');`,
           }}
-        />
-
-        {/* Verification Tags */}
-        <meta
-          name="google-site-verification"
-          content="XwCW-t3PJfyPYJRWSItA6cY1Yoy3_zuBZ8N9CJEzZwc"
-        />
-        <meta
-          name="naver-site-verification"
-          content="4be26cfdfa67a9fe5f9f0b91758530293065fdb8"
+          strategy="afterInteractive" // 페이지가 상호작용 가능해진 후 스크립트 로드
         />
 
         {/* Google Adsense */}
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6915584561138880"
           crossOrigin="anonymous"
-        ></script>
+        />
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
