dd0716f6581290d40761f9edd85c9fe309ace05a
mj94 <cmoonjun11@gmail.com>
2025-08-19 23:40:16 +0900

    del 구글 태그매니저 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -146,19 +146,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           strategy="afterInteractive"
         />
 
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N255D99T');`,
-          }}
-          strategy="afterInteractive"
-        />
-
         {/* Google Adsense */}
         <Script
           async
